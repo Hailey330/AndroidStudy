3a92b88b5aa5e0798383971ae749f945a1368bf1
baegopa <baegopa@wamwam.com>
2020-07-10 18:53:15 +0900

    android 레이아웃

---
+++ myfirstapp/app/src/main/java/com/cos/myfirstapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cos.myfirstapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // onCreate() 함수 실행
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
